9c529cc9d1ad0e290eb4a053d4c891e42c5c8815
REVANTH MANIKANTA KAKARLA <revanthmanikantak@gmail.com>
2025-11-15 10:55:35 +0530

    Add files via upload

---
+++ 7 C (DATA TYPES).c
@@ -0,0 +1,13 @@
+//program to read all datatypes by assigning the values//
+#include <stdio.h>
+int main()
+{
+	int a=56;
+	float b=52.369;
+	double c=1.235698475622;
+	char d='D';
+	printf("%d\n",a);
+	printf("%f\n",b);
+	printf("%lf\n",c);
+	printf("%c",d);
+}
